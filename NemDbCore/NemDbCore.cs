ab7a006ee11fa8a7c9c51115568e34bdde760627
Mark Moosman <MMoosman3141@users.noreply.github.com>
2022-12-30 00:50:19 -0700

    Added handling for parameters, and adjusted Nuget package settings.

--- NemDbCore/NemDbCore.cs
+++ NemDbCore/NemDbCore.cs
@@ -1,7 +1,9 @@
 ﻿using NemMvvm;
 using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
+using System.Linq;
 
 namespace NemDbCore;
 
@@ -17,40 +19,59 @@ public abstract class DbBase : NotifyPropertyChanged, IDisposable {
   public abstract DbConnection GetConnection(DbSettings settings);
 
   public abstract int ExecuteQuery(string query, DbConnection connection = null);
+  public abstract int ExecuteQuery(string query, IEnumerable<DbParameter> parameters, DbConnection connection = null);
   public abstract T GetScalar<T>(string query, DbConnection connection = null);
+  public abstract T GetScalar<T>(string query, IEnumerable<DbParameter> parameters, DbConnection connection = null);
   public abstract object GetScalar(string query, DbConnection connection = null);
+  public abstract object GetScalar(string query, IEnumerable<DbParameter> parameters, DbConnection connection = null);
   public abstract void ExecuteReader(string query, Action<DbDataReader> readAction, DbConnection connection = null, CommandBehavior readBehavior = CommandBehavior.Default);
-  
-  protected int GenExecuteQuery<C>(string query, DbConnection connection) where C : DbCommand {
-    if(connection == null) {
+  public abstract void ExecuteReader(string query, IEnumerable<DbParameter> parameters, Action<DbDataReader> readAction, DbConnection connection = null, CommandBehavior readBehavior = CommandBehavior.Default);
+
+
+  protected int GenExecuteQuery<C>(string query, DbConnection connection, IEnumerable<DbParameter> parameters) where C : DbCommand {
+    if (connection is null) {
       throw _connectionNullException;
     }
 
     int affectedCount = 0;
     using (C command = (C)Activator.CreateInstance(typeof(C), query, connection)) {
+      if (parameters?.Any() ?? false) {
+        command.Parameters.AddRange(parameters.ToArray());
+      }
+
       affectedCount = command.ExecuteNonQuery();
     }
 
     return affectedCount;
   }
-  protected T GenGetScalar<T,C>(string query, DbConnection connection) where C : DbCommand {
-    if(connection == null) {
+
+  protected T GenGetScalar<T, C>(string query, DbConnection connection, IEnumerable<DbParameter> parameters) where C : DbCommand {
+    if (connection is null) {
       throw _connectionNullException;
     }
 
     object returnValue;
-    using(C command = (C)Activator.CreateInstance(typeof(C), query, connection)) {
+    using (C command = (C)Activator.CreateInstance(typeof(C), query, connection)) {
+      if (parameters?.Any() ?? false) {
+        command.Parameters.AddRange(parameters.ToArray());
+      }
+
       returnValue = command.ExecuteScalar();
     }
 
     return (T)returnValue;
   }
-  protected void GenExecuteReader<C>(string query, Action<DbDataReader> readAction, DbConnection connection, CommandBehavior readBehavior = CommandBehavior.Default) where C : DbCommand {
-    if(connection == null) {
+
+  protected void GenExecuteReader<C>(string query, IEnumerable<DbParameter> parameters, Action<DbDataReader> readAction, DbConnection connection, CommandBehavior readBehavior = CommandBehavior.Default) where C : DbCommand {
+    if (connection is null) {
       throw _connectionNullException;
     }
 
     using C command = (C)Activator.CreateInstance(typeof(C), query, connection);
+    if (parameters?.Any() ?? false) {
+      command.Parameters.AddRange(parameters.ToArray());
+    }
+
     DbDataReader reader = command.ExecuteReader(readBehavior);
 
     readAction(reader);
@@ -58,7 +79,7 @@ public abstract class DbBase : NotifyPropertyChanged, IDisposable {
 
   public void Dispose() {
     GC.SuppressFinalize(this);
-    if(ConnectionObject != null) {
+    if (ConnectionObject is not null) {
       ConnectionObject.Dispose();
       ConnectionObject = null;
     }
--- NemDbCore/SqlServer/DbSqlServer.cs
+++ NemDbCore/SqlServer/DbSqlServer.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
@@ -11,6 +13,9 @@ public class DbSqlServer : DbBase {
   }
   public DbSqlServer(DbSqlServerSettings settings) {
     ConnectionObject = GetConnection(settings);
+  }
+  public DbSqlServer(SqlConnection connection) {
+    ConnectionObject = connection;
   }
 
   public override DbConnection GetConnection(DbSettings settings) {
@@ -28,18 +33,34 @@ public class DbSqlServer : DbBase {
   }
 
   public override int ExecuteQuery(string query, DbConnection connection = null) {
-    return GenExecuteQuery<SqlCommand>(query, connection ?? ConnectionObject);
+    return GenExecuteQuery<SqlCommand>(query, connection ?? ConnectionObject, null);
+  }
+
+  public override int ExecuteQuery(string query, IEnumerable<DbParameter> parameters, DbConnection connection = null) {
+    return GenExecuteQuery<SqlCommand>(query, connection ?? ConnectionObject, parameters);
   }
 
   public override void ExecuteReader(string query, Action<DbDataReader> readAction, DbConnection connection = null, CommandBehavior readBehavior = CommandBehavior.Default) {
-    GenExecuteReader<SqlCommand>(query, readAction, connection ?? ConnectionObject, readBehavior);
+    GenExecuteReader<SqlCommand>(query, null, readAction, connection ?? ConnectionObject, readBehavior);
+  }
+
+  public override void ExecuteReader(string query, IEnumerable<DbParameter> parameters, Action<DbDataReader> readAction, DbConnection connection = null, CommandBehavior readBehavior = CommandBehavior.Default) {
+    GenExecuteReader<SqlCommand>(query, parameters, readAction, connection ?? ConnectionObject, readBehavior);
   }
 
   public override T GetScalar<T>(string query, DbConnection connection = null) {
-    return GenGetScalar<T, SqlCommand>(query, connection ?? ConnectionObject);
+    return GenGetScalar<T, SqlCommand>(query, connection ?? ConnectionObject, null);
+  }
+
+  public override T GetScalar<T>(string query, IEnumerable<DbParameter> parameters, DbConnection connection = null) {
+    return GenGetScalar<T, SqlCommand>(query, connection ?? ConnectionObject, parameters);
   }
 
   public override object GetScalar(string query, DbConnection connection = null) {
-    return GenGetScalar<object, SqlCommand>(query, connection ?? ConnectionObject);
+    return GenGetScalar<object, SqlCommand>(query, connection ?? ConnectionObject, null);
+  }
+
+  public override object GetScalar(string query, IEnumerable<DbParameter> parameters, DbConnection connection = null) {
+    return GenGetScalar<object, SqlCommand>(query, connection ?? ConnectionObject, parameters);
   }
 }
